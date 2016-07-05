18a57a5b3936948ad60e6843e70bcda483281ae1
BlackyPaw <developers@blackypaw.com>
2016-07-05 15:44:00 +0200

    Fixed wrong usage of JSON chat formatting which resulted in message formatting to be lost on linebreaks due to the client's chat window width.

--- i18n-spigot/src/main/java/com/blackypaw/mc/i18n/I18NUtilities.java
+++ i18n-spigot/src/main/java/com/blackypaw/mc/i18n/I18NUtilities.java
@@ -20,6 +20,7 @@ import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.ComponentConverter;
 import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import com.comphenix.protocol.wrappers.nbt.NbtBase;
@@ -28,6 +29,7 @@ import com.comphenix.protocol.wrappers.nbt.NbtFactory;
 import com.comphenix.protocol.wrappers.nbt.NbtType;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -372,7 +374,18 @@ public class I18NUtilities extends JavaPlugin {
 					// Yes, the test of instance equality is what I want to do here
 					// as it saves me time determining whether the message was actually
 					// translated or not:
-					packet.getChatComponents().write( 0, WrappedChatComponent.fromText( translation ) );
+
+					// Issue #4:
+					//  https://github.com/BlackyPaw/I18N/issues/4
+					//
+					// Do this ugly conversion thing here in order to enforce usage of JSON color tags
+					// over the legacy chat format. Invoking WrappedChatComponent.fromText( ... ) for
+					// example, would result in a JSON such as {"text":"§cExample"} which can, if long enough
+					// and automatically put onto a new line by Minecraft, lose its formatting on the addtional
+					// line of chat. This issue does only arise for chat currently as it is the only place where
+					// multiple lines are even supported. In case this issue should arise again somewhere else
+					// one will simply have to do this conversion there, too:
+					packet.getChatComponents().write( 0, ComponentConverter.fromBaseComponent( TextComponent.fromLegacyText( translation ) ) );
 				}
 			}
 		} );
